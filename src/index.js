63feaa14d6fb2793ed0e5502ed716ed2d4557c6e
Napat <napat.wongchr@gmail.com>
2020-11-30 22:01:45 +0700

    feat: add prod build and import example

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import { add, subtract } from "./helpers"
+
+const addResults = add(20, 1)
+const subtractResults = subtract(20, 1)
+
+console.log('hello word')
+console.log('addResults: ', addResults)
+console.log('subtractResults: ', subtractResults)
